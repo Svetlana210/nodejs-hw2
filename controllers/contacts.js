7b4e83f9109be7364bc8766200f4660cac75c52c
Svetlana210 <svetik.may24@gmail.com>
2023-04-15 11:40:00 +0200

    hw4

---
+++ controllers/contacts.js
@@ -0,0 +1,67 @@
+const { Contact } = require("../models/contact");
+const { ctrlWrapper } = require("../utils");
+
+const { HttpError } = require("../helpers");
+
+const getAllContacts = async (req, res) => {
+  const { _id: owner } = req.user;
+  const { page = 1, limit = 20 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Contact.find({ owner }, "name email phone", {
+    skip,
+    limit,
+  }).populate("owner", "name email");
+  res.status(200).json(result);
+};
+
+const getContactById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findById(contactId);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.status(200).json(result);
+};
+
+const addBook = async (req, res) => {
+  const { _id: owner } = req.user;
+  const result = await Contact.create({ ...req.body, owner });
+  res.status(201).json(result);
+};
+const deleteContactById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndDelete(contactId);
+  if (!result) {
+    throw HttpError(404, "Contact not found");
+  }
+  res.json({ message: "Contact deleted" });
+};
+const updateContactById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.status(200).json(result);
+};
+
+const addFavorite = async (req, res, next) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.status(200).json(result);
+};
+module.exports = {
+  getAllContacts: ctrlWrapper(getAllContacts),
+  getContactById: ctrlWrapper(getContactById),
+  addBook: ctrlWrapper(addBook),
+  updateContactById: ctrlWrapper(updateContactById),
+  deleteContactById: ctrlWrapper(deleteContactById),
+  addFavorite: ctrlWrapper(addFavorite),
+};
